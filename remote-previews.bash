117bad5c22f1dcc3c27a6064f49ab343931ba9c4
rolfen <r10@eebe.be>
2023-09-04 03:53:42 +0000

    quiet

--- remote-previews.bash
+++ remote-previews.bash
@@ -59,7 +59,7 @@ echo "Please wait for previews"
 echo "Please wait for raw digests"
 ./previews-rawdigest.bash -q -c -s $tmpdir -d $dstdir
 find $tmpdir -type f \( -iname "*.ORF" -o -iname "*.ARW" \) -size +1 > >( tee >(wc -l | xargs echo "In cam JPEGS truncated:") >(while read file; do Truncate `echo "$file" | sed 's/\(.*\)\..*/\1.JPG/'`; done) > /dev/null ) 
-find $tmpdir -type f \( -iname "*.ORF" -o -iname "*.ARW" \) -size +1 > >( tee >(wc -l | xargs echo "Originals truncated:") >(while read file; do Truncate "$file" ; done) ) 
+find $tmpdir -type f \( -iname "*.ORF" -o -iname "*.ARW" \) -size +1 > >( tee >(wc -l | xargs echo "Originals truncated:") >(while read file; do Truncate "$file" ; done) > /dev/null ) 
 echo "Please wait for video previews"
 ./previews-vid.bash -b -s $tmpdir -d $dstdir
-find $tmpdir -type f \( -iname "*.MTS" -o -iname "*.AVI" -o -iname "*.MOV" -o -iname "*.MP4" \) -size +1 > >(tee >(wc -l | xargs echo "Videos truncated:") >(while read file; do Truncate "$file" ; done)) 
+find $tmpdir -type f \( -iname "*.MTS" -o -iname "*.AVI" -o -iname "*.MOV" -o -iname "*.MP4" \) -size +1 > >(tee >(wc -l | xargs echo "Videos truncated:") >(while read file; do Truncate "$file" ; done) > /dev/null ) 
